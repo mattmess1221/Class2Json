e4c97b75b71d14f881f1d31e3ada7f40e5e6de89
Matthew Messinger <mattmess1221@gmail.com>
2015-07-10 16:01:34 -0400

    Add some command line options for output file and target jar. Include all dependencies in alljar. Use proguard to shink guava.

--- src/main/java/mnm/c2j/Class2Json.java
+++ src/main/java/mnm/c2j/Class2Json.java
@@ -5,8 +5,12 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Writer;
+import java.util.Enumeration;
 import java.util.List;
 import java.util.Map;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.zip.ZipEntry;
 
 import org.apache.logging.log4j.LogManager;
 import org.objectweb.asm.ClassReader;
@@ -29,12 +33,88 @@ public abstract class Class2Json<T> {
     private Map<String, PackageJson> processed = Maps.newHashMap();
 
     public static void main(String[] args) throws IOException {
-        Class2Json<String> c2j = new StringClass();
+        List<String> classList = Lists.newArrayList();
+        File jarFile = null;
+        File outFile = new File("output.json");
+        boolean help = false;
+        String flag = null;
         for (String s : args) {
+            if (flag == null) {
+                if (s.equals("-help") || s.equals("-?")) {
+                    help = true;
+                } else if (s.startsWith("-")) {
+                    flag = s;
+                } else {
+                    classList.add(s);
+                }
+            } else if (flag.equals("-jar")) {
+                jarFile = new File(s);
+                flag = null;
+            } else if (flag.equals("-out")) {
+                outFile = new File(s);
+                flag = null;
+            }
+        }
+
+        if (help || jarFile == null && classList.isEmpty()) {
+            printUsage();
+        } else if (jarFile != null) {
+            JarFile jar = null;
+            try {
+                jar = new JarFile(jarFile);
+                List<InputStream> inputs = Lists.newArrayList();
+                if (!classList.isEmpty()) {
+                    // process selected classes
+                    for (String name : classList) {
+                        inputs.add(jar.getInputStream(new ZipEntry(name.replace(".", "/") + ".class")));
+                    }
+                } else {
+                    // process all classes
+                    Enumeration<JarEntry> enries = jar.entries();
+                    while (enries.hasMoreElements()) {
+                        JarEntry entry = enries.nextElement();
+                        if (entry.getName().endsWith(".class")) {
+                            inputs.add(jar.getInputStream(entry));
+                        }
+                    }
+                }
+                process(new StreamClass(), inputs, outFile);
+            } finally {
+                try {
+                    if (jar != null) {
+                        jar.close();
+                    }
+                } catch (IOException e) {
+                    // ignore quietly
+                }
+            }
+        } else {
+            process(new StringClass(), classList, outFile);
+        }
+    }
+
+    private static <T> void process(Class2Json<T> c2j, Iterable<T> args, File output) throws IOException {
+        for (T s : args) {
             c2j.addToProcess(s);
         }
         c2j.process();
         c2j.saveFile(new File("output.json"));
+    }
+
+    private static void printUsage() {
+        System.out.println("Usage: (<class names>... | -jar <jar file> [<class names>...]) -out <output file>");
+        System.out.println("When not using a jar, class names are from the classpath.");
+        System.out.println("If no class names are provided with a jar, all class files are processed.");
+        System.out.println();
+        System.out.println("Options:");
+        System.out.println("-help or -?\tShows this help screen");
+        System.out.println("-jar <file>\tThe jar to process");
+        System.out.println("-out <file>\tThe output file (default: output.json)");
+        System.out.println();
+        System.out.println("Examples:");
+        System.out.println("\t-out output.json java.util.List java.util.Map java.util.Map$Entry");
+        System.out.println("\t-jar example.jar -out output.json");
+        System.out.println("\t-jar example.jar com.example.ExampleClass -out example_class.json");
     }
 
     /**
